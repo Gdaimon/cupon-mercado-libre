d3271880e3f118ba9861a5705a9e0714ebccf462
Carlos Andres <darkklitos@gmail.com>
2020-06-25 01:03:57 -0500

    [add] Principal files

---
+++ src/main/java/com/mercado/com/CuponAppApplication.java
@@ -0,0 +1,21 @@
+package com.mercado.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuponAppApplication {
+	
+	public static void main ( String[] args ) {
+		SpringApplication.run ( CuponAppApplication.class, args );
+//		SpringApplication app = new SpringApplication ( CuponAppApplication.class );
+//		app.setBannerMode ( Banner.Mode.OFF );
+//		app.run ( args );
+	}
+
+//	@Override
+//	public void run ( String... args ) throws Exception {
+//		System.out.println ( "Entro" );
+//	}
+	
+}
